037727978020ee35e736fdf95a8ef61651fbe38b
Jamie Stevens <ste616@gmail.com>
2020-02-16 19:53:37 +1100

    Start making the codebase

---
+++ atca_status_tile/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "1.0.0"
+__description__ = "LiFX Tile Visualiser for ATCA status."
+__url__ = "https://github.com/ste616/atca-status-tile"
+__author__ = "Jamie Stevens"
+__authoremail__ = "ste616@gmail.com"
+__license__ = "GPLv3"
+
---
+++ atca_status_tile/status_tile.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# status_tile.py
+# Author: Jamie Stevens
+# This file contains a StatusTile class which can be used to wrap a number of
+# indicators.
+
