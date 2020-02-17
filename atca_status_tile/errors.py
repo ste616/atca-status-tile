8bfdef52df950dd6a50e37000bfa466e78ca9004
Jamie Stevens <ste616@gmail.com>
2020-02-17 20:18:51 +1100

    Start working on the tile class and associated errors.

---
+++ atca_status_tile/errors.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+# errors.py
+# Author: Jamie Stevens
+# Errors used throughout the classes.
+
+# Our custom error.
+class StatusError(Exception):
+  ## Base class for exceptions in this module.
+  pass
+
+class ArgumentError(StatusError):
+  ## Exception raised when the arguments passed to a function
+  ## are not proper or sufficient.
+  ## Attributes:
+  ##     routine: the name of the routine called
+  ##     arg: the argument specified incorrectly
+  ##     message: explanation of what the argument should be
+  def __init__(self, routine, arg, message):
+    self.routine = routine
+    self.arg = arg
+    self.message = message
+
+  def __str__(self):
+    return ("ArgumentError: %s passed to %s invalid, %s" %
+            (self.arg, self.routine, self.message))
+
+class PixelError(StatusError):
+  ## Exception raised when a pixel passed to a funciton is
+  ## not valid in some way.
+  ## Attributes:
+  ##     routine: the name of the routine called
+  ##     x: x value
+  ##     y: y value
+  ##     message: explanation of the problem
+  def __init__(self, routine, x, y, message):
+    self.routine = routine
+    self.x = x
+    self.y = y
+    self.message = message
+
+  def __str__(self):
+    return ("PixelError: in routine %s, pixel (%d, %d), %s" %
+            self.routine, self.x, self.y, self.message)
+  
--- atca_status_tile/status_tile.py
+++ atca_status_tile/status_tile.py
@@ -4,3 +4,64 @@
 # This file contains a StatusTile class which can be used to wrap a number of
 # indicators.
 
+from .errors import ArgumentError, PixelError
+
+## A mapper between (x,y) and pixel number.
+def xy2pix(x=0, y=0):
+  p = x + 8 * y
+  return p
+
+class StatusTile:
+  def __init__(self, tileNumber=None):
+    self.tileNumer = tileNumber
+    self.indicators = []
+    self.pixelsUsed = [ False ] * 64
+    self.lastBrightness = 32768
+
+  def addIndicator(self, indicator=None, x=[], y=[]):
+    p = []
+    if indicator is None:
+      raise ArgumentError(
+        routine="StatusTile.addIndicator",
+        arg="indicator",
+        message="must be an StatusIndicator object"
+        )
+    if ((len(x) == 0) or (len(y) == 0) or (len(x) != len(y))):
+      raise ArgumentError(
+        routine="StatusTile.addIndicator",
+        arg="x/y",
+        message="must be equal length lists of pixel coordinates"
+        )
+    for i in range(0, len(x)):
+      if (x[i] < 0 or x[i] > 7 or y[i] < 0 or y[i] > 7):
+        raise PixelError(
+          routine="StatusTile.addIndicator",
+          x=x[i], y=y[i],
+          message="pixel value out of range 0 <= v < 8"
+          )
+      tp = xy2pix(x[i], y[i])
+      if (self.pixelsUsed[p] == True):
+        raise PixelError(
+          routine="StatusTile.addIndicator",
+          x=x[i], y=y[i],
+          message="pixel already allocated to another indicator"
+          )
+      p.append(tp)
+    ## If we get here, this indicator can be added to our list.
+    self.indicators.append({
+      indicator: indicator,
+      pixels: tp
+      })
+    for i in p:
+      pixelsUsed[i] = True
+    return self
+
+  def refresh(self, brightness=None):
+    ## Called to update the pixel values.
+    if brightness is None:
+      brightness = self.lastBrightness
+    else:
+      self.lastBrightness = brightness
+      
+    for i in range(0, len(self.indicators)):
+      
